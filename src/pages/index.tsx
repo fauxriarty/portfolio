a4db6078c39ec78a3b6ad8f8b7a6f845ad880101
fauxriarty <aditya.chheda04@gmail.com>
2025-02-09 03:51:21 +0530

    changed orientation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,14 +6,7 @@ export const Home = () => {
   return (
     <section className="home">
         
-      <div className='content'>
-        <h1>hey. it's aditya.</h1>
-        <p>
-        I like fixing things—whether it’s code, workflows, or broken strategies. 
-        From building apps to scaling engagement, I focus on what actually works, cutting through inefficiencies
-        and making things run smoother. If it’s complex, messy, and worth solving, I’m interested.
-        </p>
-      </div>
+      
       <div className="image">
           <Image
             src="/avatar/profilepic.jpg" 
@@ -28,6 +21,14 @@ export const Home = () => {
                    250px"      
           />
         </div>
+        <div className='content'>
+        <h1>hey. it's aditya.</h1>
+        <p className='desc'>
+        I like fixing things—whether it’s code, workflows, or broken strategies. 
+        From building apps to scaling engagement, I focus on what actually works, cutting through inefficiencies
+        and making things run smoother. If it’s complex, messy, and worth solving, I’m interested.
+        </p>
+      </div>
     </section>
   );
 };
