8fd1486ad79508036fdee91c4db8ecc1cf040995
fauxriarty <aditya.chheda04@gmail.com>
2025-02-09 00:12:30 +0530

    design improvements

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -10,7 +10,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
 };
 
 // This component never actually renders, because we redirect above.
-//  next requires a default export.
+//  next requires a adefault export.
 export default function ResumeRedirect() {
   return null;
 }
