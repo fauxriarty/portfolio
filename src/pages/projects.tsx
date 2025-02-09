9242ea15ca17b18ed9a6c1c786fceda998d9c1ba
fauxriarty <aditya.chheda04@gmail.com>
2025-02-09 20:57:48 +0530

    content push

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -4,20 +4,20 @@ import './projects.scss';
 const projects = [
   { 
     id: 1, 
-    name: 'SUZI: Turning Observers into Advocates', 
-    description: `Developed an app to log geodata on needy children and poor sanitation, helping NGOs and government bodies identify and address community needs. Built the backend with Golang for API development and user authentication, and integrated heatmap functionality in Android using Kotlin and Google Maps.`,
+    name: 'SUZI: observers of change', 
+    description: `Developed an app to log and create actionable data on needy children and poor sanitation, helping NGOs and government bodies identify and address community needs. Built the backend with Golang for API development and user authentication, and integrated heatmap functionality in Android using Kotlin and Google Maps.`,
     githubUrl: 'https://github.com/suzisuzisuzi'
   },
   { 
     id: 2, 
-    name: 'Chamberly AB', 
+    name: 'Chamberly: venting made easy', 
     description: `Developed an Android version of the Chamberly App, which connects people seeking mental health support. Contributed to both backend and frontend using Kotlin and XML, optimized Cloud Firestore queries, and implemented notifications with FCM API.`,
     githubUrl: 'https://github.com/fauxriarty/Chamberly'
   },
   { 
     id: 3, 
-    name: 'CapAble: Job Search made Inclusive', 
-    description: `Created an application to help specially-abled individuals find jobs. Utilized Firebase for real-time data and user authentication, and developed a custom search functionality with dynamic filtering.`,
+    name: 'CapAble: job search made inclusive', 
+    description: `developed an application to help specially-abled individuals find jobs, scraped off of multiple sources. Utilized Firebase for real-time data and user authentication, and developed a custom search functionality with dynamic filtering, all in kotlin.`,
     githubUrl: 'https://github.com/fauxriarty/Capable'
   },
 ];
