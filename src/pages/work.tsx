1455a53f92e38e25cfbfc951f2c84b8ad02c1d55
aditya.chheda@kotak.com <aditya.chheda04@gmail.com>
2025-06-19 22:38:45 +0530

    added kotak internship

--- src/pages/work.tsx
+++ src/pages/work.tsx
@@ -6,28 +6,46 @@ const experiences = [
     id: 1, 
     title: 'Vellore Institute of Technology, Vellore', 
     description: `Bachelor of Technology in Computer Science with a spl. in data science.
-    (Expected grad: May 2026)\n\ncGPA: 9.03/10`
+(Expected grad: May 2026)
+
+CGPA: 9.03/10`
   },
   { 
-    id: 2, 
-    title: 'Google Developer Student Clubs vit - Marketing Lead', 
-    description: `• Led the Content and Digital Marketing team, nearly doubling social media followers and exponentially increasing engagement.
-    \n\n• Secured sponsorships and managed logistics and finances for multiple hackathons with 1000+ registrations.
-    \n\n• Published multiple technical blog posts on Medium, translating complex topics (like big data, deepfakes) into easily digestible narratives. `
-  } , 
+    id: 2,
+    title: 'Software Development Intern - Kotak Cherry (Kotak Bank)',
+    description: `Kotak Cherry May 2025 – Jul 2025
+Software Development Intern
+Mumbai, India
+• Authored and maintained test scripts for the Kotak Cherry investment app using Sinon and Mocha libraries in JavaScript, ensuring module reliability and correctness.
+
+• Managed test pipelines and artefacts on Azure DevOps, improving end-to-end coverage visibility and stability.`
+  },
   { 
     id: 3, 
     title: 'Full Stack Developer Intern - Drishtee Foundation', 
     description: `• Built a web application connecting people with resources and skills to those in need, supporting the 'Swavalamban' initiative through centralized Happiness Cafés.
-    \n\n• Developed the backend using Node.js, Express, Prisma ORM, and PostgreSQL, creating API endpoints for CRUD operations.
-    \n\n•Integrated Gemini API for personalized recommendations based on user skills and resources.`
+
+• Developed the backend using Node.js, Express, Prisma ORM, and PostgreSQL, creating API endpoints for CRUD operations.
+
+• Integrated Gemini API for personalized recommendations based on user skills and resources.`
   },
   { 
     id: 4, 
+    title: 'Google Developer Student Clubs VIT - Marketing Lead', 
+    description: `• Led the Content and Digital Marketing team, nearly doubling social media followers and exponentially increasing engagement.
+
+• Secured sponsorships and managed logistics and finances for multiple hackathons with 1000+ registrations.
+
+• Published multiple technical blog posts on Medium, translating complex topics (like big data, deepfakes) into easily digestible narratives.`
+  },
+  { 
+    id: 5, 
     title: 'Android Developer - Chamberly AB', 
     description: `• Developed the Android version of Chamberly (originally on iOS) using Kotlin and Android Studio.
-    \n\n• Contributed to both backend and frontend development, optimizing Cloud Firestore queries for better performance.
-    \n\n• Implemented push notifications using the FCM API.`
+
+• Contributed to both backend and frontend development, optimizing Cloud Firestore queries for better performance.
+
+• Implemented push notifications using the FCM API.`
   }
 ];
 
